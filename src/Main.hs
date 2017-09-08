eaf6f42e5c72664bad83dd6fe5c6a8c4f74fc7cc
Kelly McLaughlin <kelly.mclaughlin@joyent.com>
2017-09-08 13:29:55 -0600

    Updates

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Control.Concurrent
+-- import           Control.Concurrent
 import qualified Data.ByteString.Lazy.Char8 as L8
 import           Network.Connection
 import           Network.HTTP.Client
 import           Network.HTTP.Client.TLS
 import           Network.HTTP.Simple
+-- import           Network.HTTP.Types.Header
 
 
 main :: IO ()
@@ -16,14 +17,18 @@ main = do
                                            , managerIdleConnectionCount = 1
                                            , managerResponseTimeout = responseTimeoutNone
                                            }
-        url = "https://172.26.19.126/kelly/stor/tmp?algorithm=RSA-SHA256&expires=1503343709&keyId=%2Fkelly%2Fkeys%2F44%3A15%3A3d%3Af2%3A35%3A84%3Ab9%3A08%3Aac%3A3a%3A44%3A91%3Adc%3Ad9%3A39%3A34&signature=DUPVdZanTrbJiAQUK0mUY2EyQeNFQGwT25bDb12B6f0T88LGnyJ7yJxp59Ld6yshHVsV7YN7%2F0v3Im64sJwyPq90ouF18L40Yp5bdVcfwfqtw7ETEz9ZziybplI3yNLtUIo2%2FnRghjkVVdMlQClgsV7BAIUurVDfeo3mfrGKOhHq9SsSnbrsGMOTSqAJR0%2FlmFQs3LHhCo4KOIhLM9HQZoxfa6jPPxgree4uploz70j2Y4HjlEhAa3R1C0aezRgVVPg1DxUD4gccI9CwOSllHYmwzQ%2FyoGiGCM7%2BYCYqgva0WV%2FXrTd%2BadE3w3%2BdDvUG6DchLdfF28tXfi58ioMaCA%3D%3D"
+        url = "https://manta.orbit.example.com/kelly/stor/tmp?algorithm=RSA-SHA256&expires=1506620038&keyId=%2Fkelly%2Fkeys%2F61%3Af5%3A4a%3A9c%3Ac9%3A6b%3Aaa%3Ae4%3Ac5%3A7d%3A8d%3Ad7%3A89%3A75%3A8a%3A60&signature=hOONstgBaqieNzEN3Qm8jtWnsjxpfg55Rz2Gbxa9GNO6gHrMc1tDHyzHk%2FD8aQkDtsxcwN4MXQOqXW1iSmrK3FNcoCs7PAfnHESuwLZhPhWFX%2BmOzrryIlbuoec5BPRxcFdrEmQflitZCEMcGPk4%2BrvO3Kpi03GZ%2BGsHwEdoyU453qhkrxEplAijmUHdzRr8rcWaZicgeEmu6n%2BJYt4%2BhWQdMmsgONxBq2Fi2kQeO5md85fDZ%2F%2BkHMdFfZ%2B3C9TCIWmv5pCZ0mHeapG2uLCd3bRmBbrIwcvqPgN7G39B7Hn%2FRTmK%2B%2FTEh0b50cZZu2UfzlpEtXwrvVB1CIlnBa2Uxw%3D%3D"
+        -- url = "http://127.0.0.1:8080/test.html"
 
     manager <- newManager managerSettings'
     setGlobalManager manager
 
     initReq <- parseRequest url
     let req = initReq { method = "PUT"
-                      , requestBody = RequestBodyLBS "snarf"
+                      , requestBody = RequestBodyLBS $ L8.replicate 5000000 'a'
+                      -- , requestHeaders = [ -- (hConnection, "Close")
+                      --                      (hExpect, "100-continue")
+                      --                    ]
                       }
 
     response <- httpLBS req
@@ -32,9 +37,8 @@ main = do
                show (getResponseStatusCode response)
     print $ getResponseHeader "Content-Type" response
     L8.putStrLn $ getResponseBody response
+    -- threadDelay $ 140 * 1000000
 
-    threadDelay $ 200 * 1000000
-
-    _ <- httpLBS req
+    -- _ <- httpLBS req
 
     return ()
